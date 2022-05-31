08442a98a3c48804a6fa47faa547105d6e2a4544
Elizabetho0n <taniaorsa30@gmail.com>
2022-05-31 12:59:23 -0500

    Fin - Figuras geometricas

--- figuras.js
+++ figuras.js
@@ -6,12 +6,12 @@ console.log("Los lados del cuadrado miden:" + ladoCuadrado);
 
 function perimetroCuadrado (ladoCuadrado) {
     return ladoCuadrado * 4;
-} 
+}
 // console.log("El perimetro del cuadrado mide:" + perimetroCuadrado);
 
 function areaCuadrado (ladoCuadrado) {
     return ladoCuadrado * ladoCuadrado;
-} 
+}
 // console.log("El área del cuadrado mide:" + areaCuadrado);
 console.groupEnd("Cuadrados");
 
@@ -23,12 +23,12 @@ const ladoTriangulo1 = 6;
 const ladoTriangulo2 = 6;
 const baseTriangulo3 = 4;
 const alturaTriangulo = 7;
-console.log("Los lados del triangulo miden: " 
+console.log("Los lados del triangulo miden: "
 + ladoTriangulo1
-+ "cm, " 
-+ ladoTriangulo2 
-+ "cm, " 
-+ baseTriangulo3 
++ "cm, "
++ ladoTriangulo2
++ "cm, "
++ baseTriangulo3
 + "cm.");
 
 function perimetroTriangulo(lado1,lado2,base) {
@@ -73,3 +73,37 @@ function areaCirculo(radio) {
 
 // console.log("El área del circulo mide:" + areaCirculo);
 console.groupEnd("Circulo");
+
+
+//Interantuando con el HTML
+
+function calcularPerimetroCuadrado() {
+    const input = parseInt(document.getElementById("InputCuadrado").value);
+    const perimetro = perimetroCuadrado(input);
+    alert(perimetro);
+}
+
+function calcularAreaCuadrado() {
+    const input =document.getElementById("InputCuadrado");
+    const value = input.value;
+    const area = areaCuadrado(value);
+    alert(area);
+}
+
+//Triangulo Isoseles
+function calcularAlturaTtriangulo() {
+    const ladoUno = Number(document.getElementById("InputLadoUno").value);
+    const ladoDos = Number(document.getElementById("InputLadoDos").value);
+    const ladoBase = Number(document.getElementById("InputLadoBase").value);
+    if (ladoUno === ladoDos) {
+        const valorDentroRaiz = ladoDos * ladoDos - ((ladoBase * ladoBase)/4);
+        const resultadoAltura = Math.sqrt(valorDentroRaiz);
+        alert(resultadoAltura);
+    }
+    else {
+        alert("El valor ingresado no es de un Triángulo isósceles.")
+    }
+}
+
+
+
