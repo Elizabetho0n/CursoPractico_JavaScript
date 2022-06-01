e818a0d8355d36fb261b9d7f1317637a6bf40210
Elizabetho0n <taniaorsa30@gmail.com>
2022-06-01 07:54:27 -0500

    Finalización del proyecto - Porcentajes y descuentos

--- descuentos.js
+++ descuentos.js
@@ -4,11 +4,22 @@ const descuento = 15;
 const porcentajePrecioConDescuento = 100 - descuento;
 const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
 
-console.log({
-    precio,
-    descuento,
-    porcentajePrecioConDescuento,
-    precioConDescuento,
-})
+// Primero tenemos que traer el ID del elemento y guardarlo en una variable para poder trabajar con él.
+function calcularPrecioConDescuento() {
+    //Variables
+    const precioInput = document.getElementById("precioSinDescuento");
+    const precioValue = precioInput.value; 
 
+    const descuentoInput = document.getElementById("descuentoPorcentual");
+    const descuentoValue = descuentoInput.value;
+
+    //Operación matematica
+    const porcentajeDescontado = 100 - descuentoValue;
+    const precioFinal = (precioValue * porcentajeDescontado) / 100;
+    
+    //Para escribir en HTML desde JavaScript
+    const respuestaEnTexto = document.getElementById("ResultadoP");
+    respuestaEnTexto.innerText = "El precio a pagar actual es: " + precioFinal + ".";
+
+}
 
