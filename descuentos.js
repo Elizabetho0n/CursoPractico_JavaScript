1ef5ec6dfe6377fbceb04a587e457711a231ba34
Elizabetho0n <taniaorsa30@gmail.com>
2022-05-31 21:41:29 -0500

    Primer commit - Descuentos

---
+++ descuentos.js
@@ -0,0 +1,14 @@
+const precio = 120;
+const descuento = 15;
+
+const porcentajePrecioConDescuento = 100 - descuento;
+const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+console.log({
+    precio,
+    descuento,
+    porcentajePrecioConDescuento,
+    precioConDescuento,
+})
+
+
