7b41855601561a9ea41c7001fe81637b3149f5fd
Elizabetho0n <taniaorsa30@gmail.com>
2022-06-03 15:13:26 -0500

    Practicando el metodo .reduce

---
+++ promedio.js
@@ -0,0 +1,19 @@
+// function calculadoraPromedio(lista) {
+//     let suma = 0;
+//     for(let i = 0; i < lista.length; i++) {
+//         suma = suma + lista[i];
+//     }
+//     const promedioLista = suma / lista.length;
+//     console.log(promedioLista)
+// }
+// calculadoraPromedio([1,3,50,7])
+
+const numbers = [2,4,6,8,10];
+numbers.reduce(function(valorAnterior, valorActual){
+    
+});
+
+
+
+
+
