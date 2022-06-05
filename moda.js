7cec524ed57f68ee2411090c064b463f16ad7945
Elizabetho0n <taniaorsa30@gmail.com>
2022-06-05 11:21:46 -0500

    Finalización proyecto - moda

--- moda.js
+++ moda.js
@@ -9,7 +9,6 @@ lista1 = [
     4
 ]
 const lista1Count = {};
-
 lista1.map(
     function(elemento){
         if (lista1Count[elemento]) {
@@ -21,6 +20,18 @@ lista1.map(
 )
 console.log(lista1Count)
 
+// object entries → para convertir el objeto "lista1Count" en un array.
+const lista1Array = Object.entries(lista1Count)
+console.log(lista1Array)
 
+// metodo .sort para orednarlos de menor a mayor en base al indice
+lista1Array.sort(function(elementoA, elementoB){
+    return elementoA[1] - elementoB[1];
+})
+console.log(lista1Array)
+
+//Por último la moda va a estar en el último lugar despúes de ordenarlo porque la modad es elemento que tiene mayor frecuencia.
+const moda = lista1Array[lista1Array.length - 1]
+console.log(moda)
 
 
