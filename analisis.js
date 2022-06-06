e62c87d11c0e4b85cfcacb5ab39fccdbc978ad0a
Elizabetho0n <taniaorsa30@gmail.com>
2022-06-05 23:32:51 -0500

    avanzando

--- analisis.js
+++ analisis.js
@@ -1,29 +1,110 @@
+// const colombia = [];
+// colombia.push({
+//   name: "Camila",
+//   salary: 500,
+// });
+// colombia.push({
+//   name: "Nath",
+//   salary: 1500,
+// });
+// colombia.push({
+//   name: "Luisa",
+//   salary: 1800,
+// });
+// colombia.push({
+//   name: "Laura",
+//   salary: 1000,
+// });
+// colombia.push({
+//   name: "Daniela",
+//   salary: 2200,
+// });
+// colombia.push({
+//   name: "Esperancita",
+//   salary: 200,
+// });
+// colombia.push({
+//   name: "Carla",
+//   salary: 500,
+// });
+// colombia.push({
+//   name: "Antonieta",
+//   salary: 1500,
+// });
+// colombia.push({
+//   name: "Alicia",
+//   salary: 1300,
+// });
+// colombia.push({
+//   name: "Ana",
+//   salary: 2400,
+// });
+// colombia.push({
+//   name: "Julia",
+//   salary: 3400,
+// });
+// colombia.push({
+//   name: "Rosa",
+//   salary: 400,
+// });
+// colombia.push({
+//   name: "Angélica",
+//   salary: 400,
+// });
+// colombia.push({
+//   name: "Tatiana",
+//   salary: 400,
+// });
+// colombia.push({
+//   name: "Lorena",
+//   salary: 600,
+// });
+// colombia.push({
+//   name: "Carolina",
+//   salary: 1600,
+// });
+// colombia.push({
+//   name: "Fernanda",
+//   salary: 2600,
+// });
+// colombia.push({
+//   name: "Nora",
+//   salary: 1000,
+// });
+// colombia.push({
+//   name: "Gisselle",
+//   salary: 2000,
+// });
+// colombia.push({
+//   name: "Bill Gates",
+//   salary: 100000000,
+// });
+
+
 //El map me va a permitir traerme de la lista solo la propiedad salary
-// const colombiaSalarios = colombia.map(function(elemento){
-//     return elemento.salary;
-// })
+const colombiaSalarios = colombia.map(function(elemento){
+    return elemento.salary;
+});
 
 //Ahora voy a ordenarlos - usaré sort (es para calcular la mediana y moda).
 
-// const salariosColSorted = colombiaSalarios.sort(function(salaryA, salaryB) {
-//     return salaryA - salaryB;
-// })
+const salariosColSorted = colombiaSalarios.sort(function(salaryA, salaryB) {
+    return salaryA - salaryB;
+});
 
 //Para calcular la mediana DEPENDE de si la lista es par o impar
-const numeroLista = [
-    5, 10, 7, 3, 6, 8
-]
+
 function esPar(lista) {
-    return (lista % 2 === 0)
-}
+    return (lista % 2 === 0);
+};
 function calculadoraPromedio(lista) {
     const sumaLista = lista.reduce(function(acc = 0,actual) {
-        return acc + actual;        
+        return acc + actual;
     })
     const promedio = sumaLista / lista.length;
     return promedio;
-}
-let mediana;
+};
+
 function calcularMediana(lista) {
     const mitadLista = parseInt(lista.length / 2);
     if(esPar(lista.length)) {
@@ -31,10 +112,12 @@ function calcularMediana(lista) {
         const elementoPar2 = lista[mitadLista - 1];
 
         const mitadListaPar = calculadoraPromedio([elementoPar2, elementoPar1]);
-        mediana = mitadListaPar;
-    } else {
-        mediana = lista[mitadLista];       
-    }
-}
+        return mitadListaPar;
 
-console.log(calcularMediana(numeroLista))
+    } else {
+        const mitadListaImpar = lista[mitadLista];
+        return mitadListaImpar;
+    }
+};
+
+console.log(calcularMediana(salariosColSorted));
